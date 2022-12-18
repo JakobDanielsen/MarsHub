22e1d0e125ad5dcb47b559d14e6b655b6ca0354a
© Jakob 3301™ <danielsen.jakob@gmail.com>
2022-12-19 00:33:24 +0100

    second push
    added node express, its finally working again. just need to get the input back to the html

--- fun.js
+++ fun.js
@@ -1,28 +1,46 @@
+const express = require('express');
+const app = express();
+const https = require("https")
+const bodyParser = require("body-parser")
+const request = require("request");
+const { get } = require('request');
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname + "/index.html")
+})
+
 const API_KEY = "NxmoiyGhG7hNBxNS2pffT6ps1F7kj0w34QpoqqtP";
 // mod.cjs
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-const marspic = document.querySelector("#marspic")
+// const marspic = document.querySelector("#marspic")
 
 async function getImage(date) {
     try {
       const response = await fetch(
         `http://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${date}&camera=NAVCAM&api_key=${API_KEY}`
       );
-    //   console.log(response);
       if (!response.ok) {
         throw new Error(`Failed to fetch images: ${response.statusText}`);
       }
       const data = await response.json();
       let img = (data.photos[9].img_src);
       console.log(img);
-      marspic.src = img
     } catch (error) {
       console.error(error);
       return null;
     }
 }
 
-getImage("2016-12-12")
 
-  
+app.post("/",(req,res)=>{
+  let chosendate = req.body.dateinput;
+  getImage(chosendate)
+})
+
+app.listen(process.env.PORT || 3000,()=>{
+  console.log("port open"); 
+})
